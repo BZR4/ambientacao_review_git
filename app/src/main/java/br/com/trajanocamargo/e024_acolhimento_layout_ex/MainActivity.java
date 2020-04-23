eaa0b1fb5ac33f00f61dd182d15c4d6e30626146
Esdras Bezerra da Silva <esdrasilva@gmail.com>
2020-04-22 22:15:00 -0300

    Inclusao de funcao de evento de click. Foi adicionado uma funcao que ira controlar o clique do botao.

--- app/src/main/java/br/com/trajanocamargo/e024_acolhimento_layout_ex/MainActivity.java
+++ app/src/main/java/br/com/trajanocamargo/e024_acolhimento_layout_ex/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.trajanocamargo.e024_acolhimento_layout_ex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void minhFuncao(View view){
+        Log.d("MainActivity", " - Click");
     }
 }
